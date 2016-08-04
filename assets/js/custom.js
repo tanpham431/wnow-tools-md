00806ca2ab29d5f26b276b9037b8703b2852977b
Tan Pham <tan.pham@terralogic.com>
2016-08-04 17:51:33 +0700

    'init'

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $.material.init(); //init material layout for all website
+  $.material.ripples(); //add ripple effect when click elements
+  $.material.input(); //add ripple for input elements
+  $.material.checkbox(); // add ripple for checkbox
+  // style for dropdownlist from dropdown.js
+  $("select").dropdown({
+    "optionClass": "withripple",
+    "dropdownClass": "dropdownList"
+  });
+
+  $('.dropdownList ul').width('97%');
+
+});
