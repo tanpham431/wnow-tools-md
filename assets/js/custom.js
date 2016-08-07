fab92a81b715c87f3fc052dda5300aea8b5bf352
Phạm Minh Tân <Phm Minh Tn>
2016-08-07 17:03:13 +0700

    Completed toggle class for other fields area

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $.material.init(); //init material layout for all website
-  $.material.ripples(); //add ripple effect when click elements
-  $.material.input(); //add ripple for input elements
-  $.material.checkbox(); // add ripple for checkbox
+    $.material.init(); //init material layout for all website
+    $.material.ripples(); //add ripple effect when click elements
+    $.material.input(); //add ripple for input elements
+    $.material.checkbox(); // add ripple for checkbox
 
-  // style for dropdownlist from dropdown.js
-  $("select").dropdown({
-    "optionClass": "withripple",
-    "dropdownClass": "dropdownList"
-  });
+    // style for dropdownlist from dropdown.js
+    $("select").dropdown({
+        "optionClass": "withripple",
+        "dropdownClass": "dropdownList"
+    });
 
-  //set width of dropdown
+    //show/hide expand other fields area
+    $('#expand-other-fields-btn').click(function() {
+        $('.other-field-area').slideToggle();
+        $('#expand-other-fields-btn i').html() == "expand_more" ? $('#expand-other-fields-btn i').html('expand_less') : $('#expand-other-fields-btn i').html('expand_more');
+    });
 
 });
